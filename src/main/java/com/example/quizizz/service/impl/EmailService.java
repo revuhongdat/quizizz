95c59cc1e33999ac03daa4d4d1d07f7b4c444539
DatVu <revuhongdat@gmail.com>
2023-12-06 04:35:39 +0700

    gửi mail xác nhận duyệt giáo viên

--- src/main/java/com/example/quizizz/service/impl/EmailService.java
+++ src/main/java/com/example/quizizz/service/impl/EmailService.java
@@ -26,4 +26,12 @@ public class EmailService {
         mailMessage.setText("Tài khoản của bạn đã bị khóa. Vui lòng liên hệ quản trị viên để mở khóa tài khoản");
         javaMailSender.send(mailMessage);
     }
+    public void sendEmailApproved(String toEmail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("datvh1996@gmail.com");
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject("Thông báo xác nhận làm giáo viên");
+        mailMessage.setText("Tài khoản của bạn đã được duyệt làm giáo viên. Chúc mừng bạn!");
+        javaMailSender.send(mailMessage);
+    }
 }
