b959b869dcdaa77e686b5674a3f1a474096b6c62
setsuna1995 <133699203+setsuna1995@users.noreply.github.com>
2023-12-10 12:57:49 +0700

    update search question

--- src/main/java/com/example/quizizz/service/QuestionService.java
+++ src/main/java/com/example/quizizz/service/QuestionService.java
@@ -2,4 +2,8 @@ package com.example.quizizz.service;
 
 import com.example.quizizz.model.Question;
 
-public interface QuestionService extends GeneralService<Question>{}
+public interface QuestionService extends GeneralService<Question>{
+    Iterable<Question> findAllByCategoryQuestion_Id (Long categoryQuestion_id);
+    Iterable<Question> findAllByQuizId (Long quizId);
+
+}
