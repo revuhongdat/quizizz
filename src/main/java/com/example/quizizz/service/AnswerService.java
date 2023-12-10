221b56ffb441ef960a45ad9900a78dfcef9affb7
Nguyenhadz <nguyenha210895@gmail.com>
2023-12-10 10:56:45 +0700

    update answer

---
+++ src/main/java/com/example/quizizz/service/AnswerService.java
@@ -0,0 +1,6 @@
+package com.example.quizizz.service;
+
+import com.example.quizizz.model.Answer;
+
+public interface AnswerService extends GeneralService<Answer> {
+}
---
+++ src/main/java/com/example/quizizz/service/impl/AnswerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.quizizz.service.impl;
+
+import com.example.quizizz.model.Answer;
+import com.example.quizizz.repository.AnswerRepository;
+import com.example.quizizz.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class AnswerServiceImpl implements AnswerService {
+    @Autowired
+    AnswerRepository answerRepository;
+    @Override
+    public Iterable<Answer> findAll() {
+        return answerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Answer> findById(Long id) {
+        return answerRepository.findById(id);
+    }
+
+    @Override
+    public Answer save(Answer answer) {
+        return answerRepository.save(answer);
+    }
+
+    @Override
+    public void delete(Long id) {
+        answerRepository.deleteById(id);
+    }
+}
