041644d4f15b697094378f4b1626f46f1871bb85
DatVu <revuhongdat@gmail.com>
2023-12-18 02:22:19 +0700

    tính điểm OK

--- src/main/java/com/example/quizizz/service/AnswerService.java
+++ src/main/java/com/example/quizizz/service/AnswerService.java
@@ -1,7 +1,12 @@
 package com.example.quizizz.service;
 
 import com.example.quizizz.model.Answer;
+import com.example.quizizz.model.Question;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AnswerService extends GeneralService<Answer> {
     void deleteByContent(String content);
+    Optional<Question> findQuestionByAnswerId(Long id);
 }
