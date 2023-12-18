7af0bfc25c182cd520b676f5d0ffba3d8e3ff7ac
DatVu <revuhongdat@gmail.com>
2023-12-18 11:40:26 +0700

    Hiển thị danh sách kết quả của bài quiz theo user

--- src/main/java/com/example/quizizz/service/ResultService.java
+++ src/main/java/com/example/quizizz/service/ResultService.java
@@ -4,6 +4,8 @@ import com.example.quizizz.model.Result;
 
 public interface ResultService extends GeneralService<Result>{
     Iterable<Result> findAllByQuizId(Long id);
-    Result findAllByUserIdAndQuizIdNewest(Long idUser, Long idQuiz);
+    Iterable<Result> findAllByUserIdAndQuizId(Long idUser, Long idQuiz);
     Iterable<Result> findAllByUserId(Long idUser);
+
+    Result findByUserIdAndQuizIdAndNewest(Long idUser, Long idQuiz);
 }
